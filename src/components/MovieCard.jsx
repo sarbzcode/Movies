9890fa9b7fd6db89d61d8d780352ef333d285e45
sarb7479 <sarbzcode@gmail.com>
2025-09-28 20:58:28 -0300

    search

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -14,18 +14,26 @@ function MovieCard({movie}) {
         }
     }
 
+    function handleGoogle() {
+        const query = encodeURIComponent(movie.title + " " + movie.release_date?.split("-")[0]);
+        const url = `https://www.google.com/search?q=${query}`;
+        window.open(url, '_blank');
+    }
+
     return (
         <div className="movie-card">
-            <div className="movie-poster">
-                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-                <div className="movie-overlay">
-                    <button className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavoriteClick}>♥</button>
+            <button style={{ border: 'none', cursor: 'pointer' }} onClick={handleGoogle}>
+                <div className="movie-poster">
+                    <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+                    <div className="movie-overlay">
+                        <button className={`favorite-btn ${favorite ? "active" : ""}`} onClick={onFavoriteClick}>♥</button>
+                    </div>
                 </div>
-            </div>
-            <div className="movie-info">
-                <h3 className="movie-title">{movie.title}</h3>
-                <p className="movie-release-date">{movie.release_date?.split("-")[0]}</p>
-            </div>
+                <div className="movie-info">
+                    <h3 className="movie-title">{movie.title}</h3>
+                    <p className="movie-release-date">{movie.release_date?.split("-")[0]}</p>
+                </div>
+            </button>
         </div>
     );
 }
